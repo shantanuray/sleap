7cde90d8bd539b0ffcc684602744724c7836866f
Shantanu Ray <chico.chego@gmail.com>
2022-09-08 15:57:09 -0700

    Script to read files from dir and convert
    See sleap.io.convert

---
+++ addons/io.py
@@ -0,0 +1,88 @@
+from os.path import dirname
+import re
+
+from pathlib import PurePath
+
+from sleap import Labels, Video
+
+
+def convert(in_filename: str, out_filename: str, format: str = 'analysis', video: Video = None):
+    """`sleap-convert` function for converting .slp to different formats.
+
+    Args:
+        in_filename: Input slp file
+        out_filename: Output converted filename
+        format: Type of conversion (must match extension of out_filename)
+        video: video
+
+    Reads:
+    * SLEAP dataset in .slp, .h5, .json, or .json.zip file
+    * SLEAP "analysis" file in .h5 format
+    * LEAP dataset in .mat file
+    * DeepLabCut dataset in .yaml or .csv file
+    * DeepPoseKit dataset in .h5 file
+    * COCO keypoints dataset in .json file
+
+    Writes:
+    * SLEAP dataset (defaults to .slp if no extension specified)
+    * SLEAP "analysis" file (.h5)
+
+    You don't need to specify the input format; this will be automatically detected.
+
+    Analysis HDF5:
+
+    If you want to export an "analysis" h5 file, use `--format analysis`.
+
+    The analysis HDF5 file has these datasets:
+
+    * "track_occupancy"    (shape: tracks * frames)
+    * "tracks"             (shape: frames * nodes * 2 * tracks)
+    * "track_names"        (shape: tracks)
+    * "node_names"         (shape: nodes)
+    * "edge_names"         (shape: nodes - 1)
+    * "edge_inds"          (shape: nodes - 1)
+    * "point_scores"       (shape: frames * nodes * tracks)
+    * "instance_scores"    (shape: frames * tracks)
+    * "tracking_scores"    (shape: frames * tracks)
+
+    Note: the datasets are stored column-major as expected by MATLAB.
+    This means that if you're working with the file in Python you may want to
+    first transpose the datasets so they matche the shapes described above.
+    """
+    assert format in ['h5', 'slp', 'json', 'analysis'], f'Incorrect conversion format {format}'
+
+    video_callback = Labels.make_video_callback([dirname(in_filename)])
+    try:
+        labels: Labels = Labels.load_file(in_filename, video_search=video_callback)
+    except TypeError:
+        print("Input file isn't SLEAP dataset so attempting other importers...")
+        from sleap.io.format import read
+
+        video_path = video if video else None
+
+        labels = read(
+            in_filename,
+            for_object="labels",
+            as_format="*",
+            video_search=video_callback,
+            video=video_path,
+        )
+
+    if format == "analysis":
+        from sleap.info.write_tracking_h5 import main as write_analysis
+
+        write_analysis(
+            labels,
+            output_path=out_filename,
+            labels_path=in_filename,
+            all_frames=True,
+            video=video,
+        )
+
+    elif format in ("slp", "h5", "json"):
+        print(f"Output SLEAP dataset: {out_filename}")
+        Labels.save_file(labels, out_filename)
+
+    else:
+        print("You didn't specify how to convert the file.")
+        print(format)
---
+++ addons/utils.py
@@ -0,0 +1,34 @@
+from os.path import exists, isdir, isfile, split, join
+from os import listdir
+import re
+
+
+def extract_prefix(in_string: str, delim_pattern: str = r'\.mp4') -> str:
+    """Find delim_pattern in in_string and return prefix before delim.
+    If not match is found, return entire in_string."""
+    assert len(in_string) > 0, "Input string is empty"
+    assert len(delim_pattern) > 0, "Pattern string is empty"
+    # search for pattern in string
+    re_delim = re.search(delim_pattern, in_string)
+    if re_delim is None:
+        # pattern not found
+        out_str = in_string
+    else:
+        # extract prefix before pattern
+        out_str = in_string[:re_delim.span()[0]]
+    return out_str
+
+
+def get_files(path: str, file_pattern: str = r'.*\.slp') -> list:
+    """Find all matching files in path that match file_pattern and return list of complete file paths."""
+    assert len(path) > 0, "Path string is empty"
+    assert exists(path), "Path provided does not exist"
+    assert isdir(path), "Path provided is not a valid file directory"
+    assert len(file_pattern) > 0, "Pattern string is empty"
+    base_file_list = listdir(path)
+    out_file_list = []
+    for file in base_file_list:
+        match = re.findall(file_pattern, file)
+        if len(match) > 0:
+            out_file_list.append(join(path, file))
+    return out_file_list
